f39ec85b63775e5e810a3b39b44abeae8cd7311e
Evan Berg <evan.m.berg@gmail.com>
2016-02-14 13:55:58 -0800

    data hardcoded, gotta make it so i can push into the data array

---
+++ main.js
@@ -0,0 +1,61 @@
+var data = [
+  {id: 1, artist: "The Clash", video: <iframe width="420" height="315" src="https://www.youtube.com/embed/AL8chWFuM-s" frameborder="0" allowfullscreen></iframe>},
+  {id: 2, artist: "Pantera", video: <iframe width="420" height="315" src="https://www.youtube.com/embed/7m7njvwB-Ks" frameborder="0" allowfullscreen></iframe>}
+]
+
+var VideoPlayer = React.createClass({
+  render: function() {
+    return (
+      <div className="videoBox">
+        <h1>Videos!!</h1>
+        <ListOfVideos data={this.props.data}/>
+        <VideoForm />
+      </div>
+    );
+  }
+});
+
+var ListOfVideos = React.createClass({
+  render: function() {
+    var videoNodes = this.props.data.map(function(video) {
+      return (
+        <Video artist={video.artist} key={video.id}>
+          {video.video}
+        </Video>
+      );
+    });
+    return (
+      <div class="videoList">
+        {videoNodes}
+      </div>
+    );
+  }
+});
+
+var VideoForm = React.createClass({
+  render: function() {
+    return (
+      <div className="videoForm">
+        <h3>Sup from video form</h3>
+      </div>
+    );
+  }
+});
+
+var Video = React.createClass({
+  render: function() {
+    return (
+      <div className="video">
+        <h2 className="artist">
+          {this.props.artist}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <VideoPlayer data={data}/>,
+  document.getElementById('content')
+);
