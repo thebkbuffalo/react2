67efc1879299170ce59161eca680a7cb8b0e875f
Evan Berg <evan.m.berg@gmail.com>
2016-02-13 17:27:58 -0800

    not hooking into the db properly for some reason, fuck

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,31 @@ $(document).ready(function(){
   ];
 
   var CommentBox = React.createClass({
+    loadCommentsFromServer: function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+    getInitialState: function() {
+      return {data: []};
+    },
+    componentDidMount: function() {
+      this.loadCommentsFromServer();
+      setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
       return (
         <div className="commentBox">
           <h1>Comments</h1>
-          <CommentList data={this.props.data} />
+          <CommentList data={this.state.data} />
           <CommentForm />
         </div>
       );
@@ -63,7 +83,7 @@ $(document).ready(function(){
   });
 
   ReactDOM.render(
-    <CommentBox data={data} />,
+    <CommentBox url="/api/comments" pollInterval={2000} />,
     document.getElementById('content')
   );
 
